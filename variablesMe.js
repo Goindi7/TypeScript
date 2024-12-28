421a5cb5ca014a82020f0dfa1d7d662fa6e2a71d
Goindi7 <manpreetgoind@gmail.com>
2024-12-28 22:47:23 +0530

    init

--- variablesMe.js
+++ variablesMe.js
@@ -1,2 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Manpreet Singh Goindi";
 console.log(greetings);
+// number
+var userId = 12234;
+userId.toFixed();
+// boolean
+var isLoggedIn = false;
--- variablesMe.tsx
+++ variablesMe.tsx
@@ -2,4 +2,13 @@ let greetings: string = "Hello Manpreet Singh Goindi";
 
 console.log(greetings);
 
+
+// number
+let userId = 12234
+userId.toFixed();
+
+// boolean
+let isLoggedIn: boolean = false;
+
+// type-inference
 export{}
