c40a0e2d2cfab15f6a1f6d2fc7a4e910d58931c5
Goindi7 <manpreetgoind@gmail.com>
2024-12-29 12:40:29 +0530

    created a basic funciton and also came to know about the correct way of writing functions

---
+++ myFunctions.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    // function addTwo(num){
+    // nums.toUpperCase;
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) {
+}
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+loginUser("Manpreet", "man@gmail.com");
+signUpUser("Manpreet", "man@gmail.com", false);
+addTwo(5);
+getUpper("Manpreet");
---
+++ myFunctions.tsx
@@ -0,0 +1,23 @@
+function addTwo(num: number){
+// function addTwo(num){
+    // nums.toUpperCase;
+    return num +2;
+}
+
+function getUpper(val: String){
+    return val.toUpperCase();
+
+}
+
+function signUpUser(name: String, email: String, isPaid: boolean){
+}
+
+let loginUser = (name: String, email: String, isPaid: boolean = false) => {}
+
+loginUser("Manpreet", "man@gmail.com");
+signUpUser("Manpreet", "man@gmail.com", false)
+addTwo(5);
+getUpper("Manpreet");
+
+
+export{}
