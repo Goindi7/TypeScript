9391e813b16ca956a7db76a71bf76df5c1d502e1
Goindi7 <manpreetgoind@gmail.com>
2024-12-30 14:44:07 +0530

    FUnctions done from hitesh chaudary

--- myFunctions.tsx
+++ myFunctions.tsx
@@ -1,4 +1,4 @@
-function addTwo(num: number){
+function addTwo(num: number): number{
 // function addTwo(num){
     // nums.toUpperCase;
     return num +2;
@@ -20,4 +20,33 @@ addTwo(5);
 getUpper("Manpreet");
 
 
+function getvalue(myVal: number){
+    if(myVal > 5){
+        return true;
+    }
+    return "200 OK"
+}
+
+
+const getHello = (s:string): string =>{
+    return "Hello";
+}
+
+// const heros = ['thor', 'spiderman','ironman']
+const heros = [1, 2, 3] 
+heros.map((hero) : string=> {
+    return `hero is ${hero}`
+    // return 2
+})
+
+
+function consoleError(errmsg: string): void {
+    console.log(errmsg);
+
+}
+
+function handleError(errmsg: string): never {
+    throw new Error(errmsg);
+    
+}
 export{}
